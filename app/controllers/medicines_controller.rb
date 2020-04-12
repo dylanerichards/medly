eb142e51be241891d2745bb78f298bf07d600642
Dylan Richards <dylanerichards@gmail.com>
2020-04-11 21:21:33 -0400

    Add ability to edit, update, and delete medicines

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -4,4 +4,26 @@ class MedicinesController < ApplicationController
 
     redirect "/"
   end
+
+  get "/medicines/:id/edit" do
+    @medicine = Medicine.find(params[:id])
+
+    erb :'medicines/edit'
+  end
+
+  post "/medicines/:id" do
+    medicine = Medicine.find(params[:id])
+
+    medicine.update(params)
+
+    redirect "/"
+  end
+
+  delete "/medicines/:id" do
+    medicine = Medicine.find(params[:id])
+
+    medicine.destroy
+
+    redirect "/"
+  end
 end
